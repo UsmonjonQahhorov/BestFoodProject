66ef14172dc15b35633d43a7f8474fb44eb29f65
Usmonjon Qohhorov <qohhorovusmonjon@gmail.com>
2023-08-17 23:45:57 +0500

    modellar yozildi

--- category/models.py
+++ category/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
