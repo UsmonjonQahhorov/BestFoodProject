44784a31a22a3f4e90a601c90e3d9df732b2c3bc
Usmonjon Qohhorov <qohhorovusmonjon@gmail.com>
2023-08-18 11:14:31 +0500

    ishladi

--- orderfood/models.py
+++ orderfood/models.py
@@ -1,65 +1,55 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 from category.models import Category
 from users.models import TgUser
 from users.models import User
 
-
 class StatusChoices(models.TextChoices):
-    PREPARING = ("preparing", "Preparing")
-    DELIVERING = ("delivering", "Delivering")
-    DELIVERED = ("delivered", "Delivered")
-
+    PREPARING = "preparing", "Preparing"
+    DELIVERING = "delivering", "Delivering"
+    DELIVERED = "delivered", "Delivered"
 
 class Food(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.FloatField()
-    image = models.ImageField()
-    category = models.ForeignKey(Category,related_name="category-food",on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='food_images/')  # Example path, adjust as needed
+    category = models.ForeignKey(Category, related_name="category_foods", on_delete=models.CASCADE)
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name = "Foods"
+        verbose_name = "Food"
         verbose_name_plural = "Foods"
         ordering = ["name"]
 
     def __str__(self):
-        return f"{self.name}"
-
-
-
+        return self.name
 
 class Order(models.Model):
     total_price = models.FloatField()
-    lat = models.DecimalField()
-    lon = models.DecimalField()
+    lat = models.DecimalField(max_digits=20, decimal_places=20)
+    lon = models.DecimalField(max_digits=20, decimal_places=20)
     description = models.TextField()
-    telegram_user = models.ForeignKey(TgUser,related_name="order-telegram-user",on_delete=models.CASCADE)
-    delivered_by = models.ForeignKey(User,related_name="order-delivered",on_delete=models.CASCADE)
-    status = models.CharField(max_length=50,choices=StatusChoices.choices)
+    telegram_user = models.ForeignKey(TgUser, related_name="orders_telegram_user", on_delete=models.CASCADE)
+    delivered_by = models.ForeignKey(User, related_name="orders_delivered", on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=StatusChoices.choices)
     delivered_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name= "Orders"
+        verbose_name = "Order"
         verbose_name_plural = "Orders"
 
     def __str__(self):
-        return f"{self.description}"
-
-
-
+        return self.description
 
 class OrderFood(models.Model):
-    order  = models.ForeignKey(Order, related_name="orderfood-order",on_delete=models.CASCADE)
-    food  = models.ForeignKey(Food, related_name="orderfood-food",on_delete=models.CASCADE)
-    amount = models.IntegerField()
+    order = models.ForeignKey(Order, related_name="order_foods", on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, related_name="ordered_foods", on_delete=models.CASCADE)
+    amount = models.IntegerField(validators=[MinValueValidator(1)])
     price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-print("hello")
--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class RoleChoices(models.TextChoices):
 class User(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    role = models.CharField(max_lengh=50, choices=RoleChoices.choices)
+    role = models.CharField(max_length=50, choices=RoleChoices.choices)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
